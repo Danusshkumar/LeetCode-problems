79ed61cba13c0e0e7228421788b1ad0c4a636cd2
Danusshkumar <112367768+Danusshkumar@users.noreply.github.com>
2023-08-28 02:38:20 +0530

    Update Two sum.java

--- Two sum.java
+++ Two sum.java
@@ -1,14 +1,25 @@
 /*
 LeetCode problem : 1
 Problem statement : Two sum
-Approach: The solution is based on the two pointers approach.
+Explanation: 
+The solution is based on the two pointers approach.
 Inorder to make some logic, we have to sort the array. Sorting the array with Arrays.sort() function in java would take O(n * logn) time complexity.
+
 Now set the pointers on left and right side of the sorted array.
 If the current values' sum is equal to target then return 
 else if current values' sum is less than target then
   we have to increase the sum value, so we increment the left pointer
 else if current values' sum is greater than target then
   we have to decrease the sum value, so we decrement the right pointer
+
+We have to understand the logic behind this approach:
+Let's say arr[i] + arr[j] > target, 
+arr[i] is the smallest element when compared to other elements in the right. So arr[j] doesn't even pair with these smaller element,
+so it can't pair with any other element right to it. So we can safely ignore the arr[j] and decrement the j pointer as j--
+Similarly if
+arr[i] + arr[j] < target,
+arr[j] is the bigger element so far and arr[i] can't able make pair means, it definitely won't make pair with arr[j-1] or arr[j-2], so that 
+we can safely ignore arr[i] and can increment i pointer as i++
 */
 
 class Solution {

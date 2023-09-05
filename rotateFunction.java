e6c4144aafad02bdea0c800ff6a1a3b601dcc774
Danusshkumar <112367768+Danusshkumar@users.noreply.github.com>
2023-09-06 01:25:32 +0530

    Update rotateFunction.java

--- rotateFunction.java
+++ rotateFunction.java
@@ -1,5 +1,16 @@
 /*
 
+396. Rotate Function
+
+You are given an integer array nums of length n.
+
+Assume arrk to be an array obtained by rotating nums by k positions clock-wise. We define the rotation function F on nums as follow:
+
+F(k) = 0 * arrk[0] + 1 * arrk[1] + ... + (n - 1) * arrk[n - 1].
+Return the maximum value of F(0), F(1), ..., F(n-1).
+
+The test cases are generated so that the answer fits in a 32-bit integer.
+
 Explanation : 
 The core idea of this solution is to use the value sum.
 Let me explain in detail.
